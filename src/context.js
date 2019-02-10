f0fc2ba98a2b030264d559b98541cffeca1c656f
ltann <leeroyleeroy18@gmail.com>
2019-02-10 01:04:51 -0800

    Refactor context api

--- src/context.js
+++ src/context.js
@@ -7,8 +7,22 @@ const ProductContext = React.createContext() //create context api
 
 class ProductProvider extends Component {
     state = {
-        products: storeProducts,
+        products: [],
         detailProduct: detailProduct
+    }
+    componentDidMount() {
+      this.setProducts()
+    }
+
+    setProducts = () => {
+      let tempProducts = [];
+      storeProducts.forEach(item => {
+        const singleItem = {...item};
+        tempProducts = [...tempProducts, singleItem]
+      })
+      this.setState(() => {
+        return { products: tempProducts }
+      })
     }
 
     handleDetail = () => {
