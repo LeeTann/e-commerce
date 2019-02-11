6d94221b1cf0c9bc19813c647852353a53f39cc0
ltann <leeroyleeroy18@gmail.com>
2019-02-10 20:38:30 -0800

    Added ability to delete each item

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -7,13 +7,13 @@ export default function CartTotals({ value }) {
     <React.Fragment>
         <div className="container">
             <div className="row">
-                <div className="col-10 mt-2 ml-ms-5 ml-md-auto col-sm-8">
+                <div className="col-10 mt-2 ml-ms-5 ml-md-auto col-sm-8 text-capitalize text-right">
                     <Link to="/">
                         <button 
                             className="btn btn-outline-danger text-uppercase mb-3 px-5"
                             type="button"
                             onClick={() => clearCart()}>
-                            clear cart
+                            empty cart
                         </button>
                     </Link>
                     <h5>
